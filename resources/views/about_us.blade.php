b539ac654d3586d8c8c9f1da886a8bb7d5cadfaa
Gul Faraz <gulfarazkhanniazi3@gmail.com>
2025-06-14 12:59:12 +0500

    Navbar Copleted

---
+++ resources/views/about_us.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Laravel App</title>
+    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+</head>
+<body>
+    <x-navbar />
+</body>
+</html>
